e702388aa7b7e048904c7a99ae4ccafd16ba4bfe
Ovi Isai <ovidiu.isai@sixt.com>
2015-09-23 09:42:19 +0200

    adding a simlink

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,7 +1,7 @@
 
 class springiotools {
 	
-	$tar_filename = 'spring-tool-suite-3.7.0.RELEASE-e4.5-linux-gtk-x86_64.tar.gz'
+    $tar_filename = 'spring-tool-suite-3.7.0.RELEASE-e4.5-linux-gtk-x86_64.tar.gz'
 
 	exec {"wget_springtoolsuite":
             command => "/usr/bin/wget --progress=bar http://dist.springsource.com/release/STS/3.7.0.RELEASE/dist/e4.5/${tar_filename}",
@@ -14,12 +14,17 @@ class springiotools {
             command => "/bin/tar -xzf /tmp/$tar_filename",
             unless  => "/usr/bin/test -d /opt/sts-bundle",
             cwd     => "/opt",
-            require => Exec["wget_springtoolsuite"],	
+            require => Exec["wget_springtoolsuite"]	
     }
 
 	file {"/tmp/$tar_filename":
 		ensure => absent,
 		require => Exec["extract_springtoolsuite"]
 	}
+
+  file { '/opt/sts-bundle/sts-home':
+   ensure => 'link',
+   target => '/opt/sts-bundle/sts-3.7.0.RELEASE'
+}
 
 }
