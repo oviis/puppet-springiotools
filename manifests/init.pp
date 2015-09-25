337ecfa5707d7e3db40b46f10f703a371371cd9c
Ovi Isai <ovidiu.isai@sixt.com>
2015-09-25 10:11:26 +0200

    adding the change owner ressource

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,30 +1,35 @@
 
 class springiotools {
 	
-    $tar_filename = 'spring-tool-suite-3.7.0.RELEASE-e4.5-linux-gtk-x86_64.tar.gz'
+  $tar_filename = 'spring-tool-suite-3.7.0.RELEASE-e4.5-linux-gtk-x86_64.tar.gz'
 
-	exec {"wget_springtoolsuite":
-            command => "/usr/bin/wget --progress=bar http://dist.springsource.com/release/STS/3.7.0.RELEASE/dist/e4.5/${tar_filename}",
-            unless  => "/usr/bin/test -f /tmp/${tar_filename}",
-            cwd     => "/tmp"
-            #timeout => 0
-    }
+  exec {"wget_springtoolsuite":
+    command => "/usr/bin/wget --progress=bar http://dist.springsource.com/release/STS/3.7.0.RELEASE/dist/e4.5/${tar_filename}",
+    unless  => "/usr/bin/test -f /tmp/${tar_filename}",
+    cwd     => "/tmp"
+  }
 
-	exec {"extract_springtoolsuite":
-            command => "/bin/tar -xzf /tmp/$tar_filename",
-            unless  => "/usr/bin/test -d /opt/sts-bundle",
-            cwd     => "/opt",
-            require => Exec["wget_springtoolsuite"]	
-    }
+  exec {"extract_springtoolsuite":
+    command => "/bin/tar -xzf /tmp/$tar_filename",
+    unless  => "/usr/bin/test -d /opt/sts-bundle",
+    cwd     => "/opt",
+    require => Exec["wget_springtoolsuite"]	
+  }
 
-	file {"/tmp/$tar_filename":
-		ensure => absent,
-		require => Exec["extract_springtoolsuite"]
-	}
+  exec {"change_own":
+    command => "/bin/chown -R vagrant:root /opt/sts-bundle/sts-home/ ",
+    cwd     => "/opt",
+    require => Exec["extract_springtoolsuite"] 
+  }
+
+#  file {"/tmp/$tar_filename":
+ #   ensure => absent,
+ #  require => Exec["extract_springtoolsuite"]
+#}
 
   file { '/opt/sts-bundle/sts-home':
-   ensure => 'link',
-   target => '/opt/sts-bundle/sts-3.7.0.RELEASE'
-}
+    ensure => 'link',
+    target => '/opt/sts-bundle/sts-3.7.0.RELEASE'
+  }
 
 }
